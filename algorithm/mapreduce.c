ccef836b62c244c119954f66a7d5dadda5546281
ruang <longjinyii@Outlook.com>
2023-03-06 18:15:29 +0800

    更新了mapreduce算法原型

--- algorithm/mapreduce.c
+++ algorithm/mapreduce.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <malloc.h>
 #include "mapreduce.h"
 
 int main(int argc, char const *argv[]){
-    char *data="hello hallow asd qwec adq zca";
-    Mapper(data);
+    KV *head = Init();
+    Mapper("hello world");
+    
     return 0;
 }
 
 KV *Mapper(char *map){
-    printf("%s ",map);
+    char tmp[16];
+    for(char *m=map,i=0;*m!=0;m+=1,i++){
+        tmp[i] = *m;
+        if((int)(*m) == 32){
+            // printf("%s ",tmp);
+        }
+    }
     return NULL;
 }
 
@@ -18,4 +26,24 @@ KV *GroupByKey(KV *group){
 
 KV *Reduce(KV * reduce){
     return NULL;
+}
+
+static int Insert(KV *head,char *key,int value){
+    KV *h_node = head;
+    if(head==NULL){ return 0; }
+    while(h_node->next!=NULL){ h_node = h_node->next; }
+    h_node->next = (KV*)malloc(sizeof(KV));
+    h_node = h_node->next;
+    h_node->key = key;
+    h_node->value = value;
+    h_node->next = NULL;
+    return 1;
+}
+
+static KV *Init(void){
+    KV *head = (KV*)malloc(sizeof(KV));
+    head->key = NULL;
+    head->value = 0;
+    head->next=NULL;
+    return head;
 }
--- algorithm/mapreduce.h
+++ algorithm/mapreduce.h
@@ -1,14 +1,19 @@
 #ifndef algorithm_mapreduce_h
 #define algorithm_mapreduce_h
 
+#include <stdio.h>
+#include <malloc.h>
 typedef struct __KV{
     char *key;
-    void *value;
+    int value;
+    struct __KV *next;
 }KV;
 
-KV *Mapper(char *);
-KV *GroupByKey(KV *);
-KV *Reduce(KV *);
 
+KV *Mapper(char *); 
+KV *GroupByKey(KV *); // sort or partition KV paris 
+KV *Reduce(KV *); // reduce KV paris and output some result
+static int Insert(KV *,char *,int); // add KV paris to linkedlist 
+static KV *Init(void);
 
 #endif // algorithm_mapreduce_h
